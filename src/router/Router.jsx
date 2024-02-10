1aad1bd7fdca9a31d5f336592c84573bba7312fa
darkhacker1906 <singhabhinandan634@gmail.com>
2024-02-10 15:44:41 +0530

    Resolved the bugs

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import RegistrationPage from "../pages/RegistrationPage";
 import FormTable from "../pages/FormTable";
 import UserData from "../components/UserData";
-import LogIn from "../pages/LogIn";
 import LoginDashboard from "../pages/LoginDashboard";
 
 function Router() {
@@ -14,7 +13,6 @@ function Router() {
         <Route path="/register" element={<RegistrationPage />}></Route>
         <Route path="/form-table" element={<FormTable />}></Route>
         <Route path='/form-table/:id' element={<UserData/>}></Route>
-        {/* <Route path='/login' element={<LogIn/>}/> */}
  
       </Routes>
     </div>
